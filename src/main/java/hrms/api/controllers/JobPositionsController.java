9559d7d4d36fa55f85c1aec049763d151a9b2749
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-11-09 12:48:34 +0100

    style: edit import file

--- src/main/java/hrms/api/controllers/JobPositionsController.java
+++ src/main/java/hrms/api/controllers/JobPositionsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,6 @@ import hrms.business.abstracts.JobPositionsService;
 import hrms.core.utils.results.DataResult;
 import hrms.core.utils.results.Result;
 import hrms.entities.concretes.JobPosition;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @CrossOrigin
