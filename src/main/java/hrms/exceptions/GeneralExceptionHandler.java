69347a88478f07d222a880b72ca3f48dd8254f31
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-11-24 11:39:45 +0100

    added general exception

---
+++ src/main/java/hrms/exceptions/GeneralExceptionHandler.java
@@ -0,0 +1,33 @@
+package hrms.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.validation.FieldError;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import hrms.core.utils.results.ErrorDataResult;
+
+@RestControllerAdvice
+public class GeneralExceptionHandler {
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){
+        Map<String, String> validationErrors = new HashMap<String,String>();
+        
+        for (FieldError fieldError : exceptions.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        ErrorDataResult<Object> errors = new ErrorDataResult<Object>(validationErrors, "Verification Error");
+
+        return errors;
+    } 
+
+}
