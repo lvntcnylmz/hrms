406a95ee8411e3b7c91a225f8239eb39c746cb5c
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-12-06 14:29:02 +0100

    refactor : enable security auto configuration

--- src/main/java/hrms/DemoApplication.java
+++ src/main/java/hrms/DemoApplication.java
@@ -1,9 +1,9 @@
 package hrms;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -11,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+@EnableAutoConfiguration
 public class DemoApplication {
 
     public static void main(String[] args) {
