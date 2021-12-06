e9e7be2d8e7058ccbdf8608d620e12b050e00deb
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-12-06 14:31:29 +0100

    feat : add spring-security and auth manager for users

--- src/main/java/hrms/core/security/SecurityConfiguration.java
+++ src/main/java/hrms/core/security/SecurityConfiguration.java
@@ -2,14 +2,16 @@ package hrms.core.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -18,21 +20,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/add").hasAuthority("ADMIN")
-                .antMatchers("/jobSeekers/getAll").hasAnyAuthority("ADMIN", "USER")
-                .antMatchers("/").permitAll()
-                .and()
-                .formLogin();
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/getAll").permitAll()
+//                .antMatchers("/add").hasRole("ADMIN")
+//                .and()
+//                .httpBasic();
+//    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(this.userDetailsService);
+
+        return daoAuthenticationProvider;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/hrms/core/security/UserDetailManager.java
+++ src/main/java/hrms/core/security/UserDetailManager.java
@@ -8,8 +8,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class UserDetailManager implements UserDetailsService {
 
@@ -19,9 +17,17 @@ public class UserDetailManager implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        Optional<User> user = this.userDao.findByEmail(email);
-        user.orElseThrow(() -> new UsernameNotFoundException("User not found by email"));
+        User user = this.userDao.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found by email.");
+        }
 
-        return user.map(UserRequestDetail::new).get();
+        //return user.map(UserRequestDetail::new).get();
+
+        UserDetails userResponse = org.springframework.security.core.userdetails.User.withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("ADMIN").build();
+
+        return userResponse;
     }
 }
