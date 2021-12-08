ea1046a80bc964b45b33400520017a3f93b9ae1f
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-12-08 20:13:24 +0100

    feat : add new exception for incorrect user login

--- src/main/java/hrms/exceptions/GeneralExceptionHandler.java
+++ src/main/java/hrms/exceptions/GeneralExceptionHandler.java
@@ -1,22 +1,16 @@
 package hrms.exceptions;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.validation.constraints.NotNull;
-
+import hrms.core.utils.results.ErrorDataResult;
 import hrms.core.utils.results.ErrorResult;
-import org.springframework.validation.FieldError;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import hrms.core.utils.results.ErrorDataResult;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GeneralExceptionHandler {
@@ -39,7 +33,12 @@ public class GeneralExceptionHandler {
     }
 
     @ExceptionHandler
-    public ErrorResult mernisInvalidUserException(MernisInvalidUserException exception){
+    public ErrorResult mernisInvalidUserException(MernisInvalidUserException exception) {
+        return new ErrorResult(exception.getLocalizedMessage());
+    }
+
+    @ExceptionHandler
+    public ErrorResult userNotFoundException(UserNotFoundException exception) {
         return new ErrorResult(exception.getLocalizedMessage());
     }
 
