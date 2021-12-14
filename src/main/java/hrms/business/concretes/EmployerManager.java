4f0f57f48173d523de886d9a278f61f9a5d989b3
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-12-14 14:02:24 +0100

    chore : add password hash to employer register

--- src/main/java/hrms/business/concretes/EmployerManager.java
+++ src/main/java/hrms/business/concretes/EmployerManager.java
@@ -1,42 +1,44 @@
 package hrms.business.concretes;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import hrms.business.abstracts.EmployerService;
 import hrms.core.utils.Business.BusinessRules;
-import hrms.core.utils.results.DataResult;
-import hrms.core.utils.results.ErrorResult;
-import hrms.core.utils.results.Result;
-import hrms.core.utils.results.SuccessDataResult;
-import hrms.core.utils.results.SuccessResult;
+import hrms.core.utils.results.*;
 import hrms.core.verifications.concretes.EmailVerification;
 import hrms.dataAccess.abstracts.EmployerDao;
 import hrms.entities.concretes.Employer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployerManager implements EmployerService {
 
-    private EmployerDao employerDao;
-    private EmailVerification emailVerification;
+    private final EmployerDao employerDao;
+    private final EmailVerification emailVerification;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public EmployerManager(EmployerDao employerDao, EmailVerification emailVerification) {
+    public EmployerManager(EmployerDao employerDao,
+                           EmailVerification emailVerification,
+                           PasswordEncoder passwordEncoder) {
         this.employerDao = employerDao;
         this.emailVerification = emailVerification;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public Result add(Employer employer) {
-        
-        var result = BusinessRules.Run(this.emailVerification.verifyEmail(employer.getEmail()), checkIfEmailExists(employer.getEmail()));
+
+        var result = BusinessRules.Run(this.emailVerification.verifyEmail(employer.getEmail()),
+                checkIfEmailExists(employer.getEmail()));
 
         if (result != null) {
             return result;
         }
 
+        employer.setPassword(this.passwordEncoder.encode(employer.getPassword()));
         return new SuccessDataResult<Employer>(this.employerDao.save(employer), "Employer information was saved.");
     }
 
@@ -45,7 +47,7 @@ public class EmployerManager implements EmployerService {
         return new SuccessDataResult<List<Employer>>(this.employerDao.findAll(), "Employers are listed.");
     }
 
-    private Result checkIfEmailExists(String email){
+    private Result checkIfEmailExists(String email) {
 
         var result = this.employerDao.existsByEmailIgnoreCase(email);
 
@@ -54,5 +56,5 @@ public class EmployerManager implements EmployerService {
         }
         return new SuccessResult("Email valid.");
     }
-    
+
 }
