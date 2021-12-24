1fca90604ed8bb30cf26b4d6671cd82239fced45
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2021-12-24 13:22:07 +0100

    add new class for jwt creation and jwt validation

---
+++ src/main/java/hrms/core/security/jwt/JwtUtil.java
@@ -0,0 +1,35 @@
+package hrms.core.security.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import hrms.core.security.AppUserDetails;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Configuration
+public class JwtUtil {
+
+    public String createToken(AppUserDetails appUserDetails) {
+        return JWT.create()
+                .withSubject(appUserDetails.getUsername())
+                .withClaim("roles", appUserDetails.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()))
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(JwtProperties.SECRET.getBytes()));
+    }
+
+    public String verifyToken(String token) {
+        return JWT.require(Algorithm.HMAC512(JwtProperties.SECRET.getBytes()))
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+
+}
+
+
