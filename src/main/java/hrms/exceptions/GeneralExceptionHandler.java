6c72849f85a024f904a4842c161e28f001458b1d
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-01-03 12:24:10 +0100

    update exception class for bean validations

--- src/main/java/hrms/exceptions/GeneralExceptionHandler.java
+++ src/main/java/hrms/exceptions/GeneralExceptionHandler.java
@@ -2,44 +2,46 @@ package hrms.exceptions;
 
 import hrms.core.utils.results.ErrorDataResult;
 import hrms.core.utils.results.ErrorResult;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GeneralExceptionHandler {
+public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException exceptions) {
+    @Override
+    protected @NotNull ResponseEntity<Object> handleMethodArgumentNotValid(@NotNull MethodArgumentNotValidException ex,
+                                                                           @NotNull HttpHeaders headers,
+                                                                           @NotNull HttpStatus status,
+                                                                           @NotNull WebRequest request) {
+        Map<String, String> validationErrors = new HashMap<>();
 
-        Map<String, String> validationErrors = new HashMap<String, String>();
-
-        for (FieldError fieldError : exceptions.getBindingResult().getFieldErrors()) {
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
             validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
 
-        return new ResponseEntity<>(new ErrorDataResult<Object>(validationErrors, "Verification Error"), HttpStatus.BAD_REQUEST);
+        ErrorDataResult<Object> errors = new ErrorDataResult<>(validationErrors, "Verifications Errors...");
 
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(JobNotFoundException.class)
-    public ResponseEntity<?> jobNotFoundException(JobNotFoundException exception) {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> jobNotFoundException(EntityNotFoundException exception) {
         return new ResponseEntity<>(new ErrorResult(exception.getLocalizedMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MernisInvalidUserException.class)
     public ResponseEntity<?> mernisInvalidUserException(MernisInvalidUserException exception) {
-        return new ResponseEntity<>(new ErrorResult(exception.getLocalizedMessage()), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<?> userNotFoundException(UserNotFoundException exception) {
         return new ResponseEntity<>(new ErrorResult(exception.getLocalizedMessage()), HttpStatus.NOT_FOUND);
     }
 
