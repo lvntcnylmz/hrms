87c8e7464e3f65e6389e232a166036364826e579
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-01-03 12:25:28 +0100

    update dto for validation

--- src/main/java/hrms/entities/dtos/request/EmployerRegisterDto.java
+++ src/main/java/hrms/entities/dtos/request/EmployerRegisterDto.java
@@ -4,15 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployerRegisterDto {
 
+    @Email
+    @NotBlank(message = "Email cannot be empty.")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty.")
     private String password;
+
+    @NotBlank(message = "Company name cannot be empty.")
     private String companyName;
+
+    @NotBlank(message = "WebSite cannot be empty.")
     private String webSite;
+
+    @NotBlank(message = "Phone number cannot be empty.")
     private String phoneNumber;
 
 }
--- src/main/java/hrms/entities/dtos/request/JobSeekerRegisterDto.java
+++ src/main/java/hrms/entities/dtos/request/JobSeekerRegisterDto.java
@@ -4,16 +4,33 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class JobSeekerRegisterDto {
 
+    @NotBlank(message = "First name cannot be empty.")
     private String firstName;
+
+    @NotBlank(message = "Last name cannot be empty.")
     private String lastName;
+
+    @NotBlank(message = "Date of birth cannot be empty.")
     private String dateOfBirth;
+
+    @Size(min = 11, max = 11)
+    @NotBlank(message = "National Id cannot be empty.")
     private String nationalId;
+
+    @Email
+    @NotBlank(message = "Email cannot be empty.")
     private String email;
+
+    @NotBlank(message = "Password cannot be empty.")
     private String password;
 
 }
