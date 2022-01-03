4e5c6fc1eaef1eeb9153709b2d78b22bd771e6c5
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-01-03 12:23:27 +0100

    update exception for all database entities

--- src/main/java/hrms/exceptions/EntityNotFoundException.java
+++ src/main/java/hrms/exceptions/EntityNotFoundException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class UserNotFoundException extends RuntimeException {
+public class EntityNotFoundException extends RuntimeException {
 
-    public UserNotFoundException(String message) {
+    public EntityNotFoundException(String message) {
         super(message);
     }
 
