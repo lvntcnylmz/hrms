04c848809c004896711c3821bf33117979a2a2e2
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-01-05 13:46:26 +0100

    refactor main class

--- src/main/java/hrms/DemoApplication.java
+++ src/main/java/hrms/DemoApplication.java
@@ -1,12 +1,7 @@
 package hrms;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -15,19 +10,6 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-        .select()
-        .apis(RequestHandlerSelectors.basePackage("hrms"))
-        .build();
-    }
-
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
     }
 
 }
