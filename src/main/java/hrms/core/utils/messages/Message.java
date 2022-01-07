1746e8a1f38d0cf743ecb542b46b1bd1b9793b95
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-01-07 13:37:02 +0100

    add messages for error handling

---
+++ src/main/java/hrms/core/utils/messages/Message.java
@@ -0,0 +1,18 @@
+package hrms.core.utils.messages;
+
+public class Message {
+
+    public static final String SAVED = "Saved";
+    public static final String DELETED = "Deleted";
+    public static final String LISTED = "Listed";
+    public static final String FOUND = "Found";
+    public static final String NOT_FOUND = "Not found by Id";
+
+    public static final String EMAIL_EXISTS = "Email already exists";
+    public static final String NATIONAL_ID_EXISTS = "National ID already exists";
+
+    public static final String INVALID_PASSWORD = "Invalid Password";
+    public static final String SUCCESSFUL_LOGIN = "Login Successful";
+    public static final String EMAIL_NOT_FOUND = "Not found by email";
+
+}
