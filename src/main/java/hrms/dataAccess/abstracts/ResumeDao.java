4cc47f4bd888cde01804218a1f4c23f15fa5c8e7
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-01-12 12:33:36 +0100

    delete old file

--- src/main/java/hrms/dataAccess/abstracts/ResumeDao.java
+++
@@ -1,9 +0,0 @@
-package hrms.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import hrms.entities.concretes.Resume;
-
-public interface ResumeDao extends JpaRepository<Resume, Integer> {
-    
-}
