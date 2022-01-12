debf9bf6c6cd6c15510419cda2cb0569c58acbc1
Levent C. Yilmaz <lvntcnylmz@hotmail.com>
2022-01-12 12:37:13 +0100

    add getAll method to manager

--- src/main/java/hrms/business/concretes/ContactManager.java
+++ src/main/java/hrms/business/concretes/ContactManager.java
@@ -2,11 +2,14 @@ package hrms.business.concretes;
 
 import hrms.business.abstracts.ContactService;
 import hrms.core.utils.messages.Message;
+import hrms.core.utils.results.DataResult;
 import hrms.core.utils.results.Result;
 import hrms.core.utils.results.SuccessDataResult;
 import hrms.dataAccess.abstracts.ContactDao;
 import hrms.entities.concretes.Contact;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ContactManager implements ContactService {
@@ -20,6 +23,11 @@ public class ContactManager implements ContactService {
     @Override
     public Result add(Contact contact) {
         return new SuccessDataResult<Contact>(this.contactDao.save(contact), Message.SAVED);
+    }
+
+    @Override
+    public DataResult<List<Contact>> getAll() {
+        return new SuccessDataResult<>(this.contactDao.findAll(), Message.LISTED);
     }
 
 }
